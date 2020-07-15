7a1001bfd1836ca3b7f4ee8e2cb9f87c68e7dceb
seakeene <56742146+seakeene@users.noreply.github.com>
2020-07-15 11:23:47 -0700

    Add files via upload

--- NaN Repair Tool.py
+++ NaN Repair Tool.py
@@ -2,7 +2,17 @@
 """
 Spyder Editor
 
-This is a temporary script file.
+NaN Repair Tool
+7/13/2020 by Sean Keene for Portland State University Power Engineering Group
+F-RESP project.
+
+This tool manually corrects an issue with F-RESP Event Files caused by having 
+two PMUs connected simultaneously.
+
+7/13 CURRENT STATUS: NaN functionality complete. Remaining work is to write
+the threshold recalculation scripts for all possible situations, and the
+Overwrite Original File functionality. Current code doesn't work for threshold
+(all outputs are zero).
 """
 
 import pandas as pd
@@ -80,7 +90,238 @@ except ValueError:
         fixed_slews = []
         for i in range(window):
             fixed_slews.append(recalculate_slew_rate(window, freq_data, nullmask[0]+i))
-            
         init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:Slew200'] = fixed_slews
     
-       
+    if 'STATION_1:OFSlew' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFSlew' in init_table.columns:
+        window = 350
+        th = 100
+        fixed_flags = []
+        for i in range(window):
+            fixed_flags.append(1 if i in init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:SlewRate'] <= th else 0)
+        init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:UFSlew'] = fixed_flags
+    
+    if 'STATION_1:OFT4' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT4' in init_table.columns:
+        window = 350
+        th = -0.004
+        fixed_flags = []
+        for i in range(window):
+            fixed_flags.append(1 if i in init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:SlewRate'] <= th else 0)
+        init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:UFT4'] = fixed_flags
+    
+    if 'STATION_1:OFT5' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT5' in init_table.columns:
+        window = 350
+        th = -0.005
+        fixed_flags = []
+        for i in range(window):
+            fixed_flags.append(1 if i in init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:SlewRate'] <= th else 0)
+        init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:UFT5'] = fixed_flags
+    
+    if 'STATION_1:OFT6' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT6' in init_table.columns:
+        window = 350
+        th = -0.006
+        fixed_flags = []
+        for i in range(window):
+            fixed_flags.append(1 if i in init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:SlewRate'] <= th else 0)
+        init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:UFT6'] = fixed_flags
+    
+    if 'STATION_1:OFT7' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT7' in init_table.columns:
+        window = 350
+        th = -0.007
+        fixed_flags = []
+        for i in range(window):
+            fixed_flags.append(1 if i in init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:SlewRate'] <= th else 0)
+        init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:UFT7'] = fixed_flags
+    
+    if 'STATION_1:OFT8' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT8' in init_table.columns:
+        window = 350
+        th = -0.008
+        fixed_flags = []
+        for i in range(window):
+            fixed_flags.append(1 if i in init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:SlewRate'] <= th else 0)
+        init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:UFT8'] = fixed_flags
+    
+    if 'STATION_1:OFT9' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT9' in init_table.columns:
+        window = 350
+        th = -0.009
+        fixed_flags = []
+        for i in range(window):
+            fixed_flags.append(1 if i in init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:SlewRate'] <= th else 0)
+        init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:UFT9'] = fixed_flags
+    
+    if 'STATION_1:OFT10' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT10' in init_table.columns:
+        window = 350
+        th = -0.01
+        fixed_flags = []
+        for i in range(window):
+            fixed_flags.append(1 if i in init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:SlewRate'] <= th else 0)
+        init_table.loc[range(nullmask[0], nullmask[0]+window),'STATION_1:UFT10'] = fixed_flags
+    
+    if 'STATION_1:OFSlew50' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFSlew50' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT450' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT450' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT550' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT550' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT650' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT650' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT750' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT750' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT850' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT850' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT950' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT950' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT1050' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT1050' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFSlew100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFSlew100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT4100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT4100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT5100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT5100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT6100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT6100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT7100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT7100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT8100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT8100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT9100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT9100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT10100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT10100' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFSlew200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFSlew200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT4200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT4200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT5200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT5200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT6200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT6200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT7200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT7200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT8200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT8200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT9200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT9200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:OFT10200' in init_table.columns:
+        pass
+    
+    if 'STATION_1:UFT10200' in init_table.columns:
+        pass
+    
+        
